a25cb4e2625d518173fbe0392a12cb9cdefa69b0
Anna Abramova <ayabram8@mts.ru>
2023-02-20 10:35:32 +0300

    Feature/dop 2092 (#3)
    * [2092] setup.py
    
    * [2092] dev release
    
    * [2092] dev release
    
    * [2092] dev release
    
    * [2092] dev release
    
    * [2092] dev release
    
    * [2092] dev release

--- setup.py
+++ setup.py
@@ -1,16 +1,32 @@
+import os
 from pathlib import Path
 
 from setuptools import find_packages, setup
 
-from version import get_version
+def get_version():
+    if os.getenv("GITHUB_REF_TYPE", "branch") == "tag":
+        return os.environ["GITHUB_REF_NAME"]
 
-HERE = Path(__file__).parent.resolve()
+    version_file = here / "evacuator" / "VERSION"
+    version = version_file.read_text().strip()  # noqa: WPS410
+
+    build_num = os.environ.get("GITHUB_RUN_ID", "0")
+    branch_name = os.environ.get("GITHUB_REF_NAME", "")
+
+    if not branch_name:
+        return version
+
+    return f"{version}.dev{build_num}"
+
+here = Path(__file__).parent.resolve()
+long_description = (here / "README.rst").read_text()
 
 setup(
     name="evacuator",
     version=get_version(),
     description="Catch an exception and exit with an exit code",
-    long_description=(HERE / "README.rst").read_text(),
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     license="Apache License 2.0",
     license_files=("LICENSE.txt",),
     url="https://github.com/MobileTeleSystems/evacuator",
@@ -22,6 +38,13 @@ setup(
     zip_safe=False,
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
     ],
     project_urls={
         "Documentation": "https://evacuator.readthedocs.io/en/stable/",
