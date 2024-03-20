73f6bfb729f42cb975af312d792f613883df907f
Мартынов Максим Сергеевич <msmarty5@mts.ru>
2024-03-20 11:50:38 +0300

    [DOP-13779] Update docs config

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,6 @@ numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-html_extra_path = ["robots.txt"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -76,6 +75,7 @@ html_theme = "furo"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_extra_path = ["robots.txt"]
 # The master toctree document.
 master_doc = "index"
 
